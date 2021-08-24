bdfdef206566b17a3d486eb8e17280fda9a0f4df
Fredrick Owiti <fotsola10@gmail.com>
2021-08-24 10:05:53 -0700

    adding a split line function

--- assist.c
+++ assist.c
@@ -19,3 +19,30 @@ char *_getline(void)
 	}
 	return (line);
 }
+/**
+ * split_line - Splits line into args
+ * @line: Line of user input
+ * Return: Array of args of user input
+ */
+char **split_line(char *line)
+{
+	size_t buffer_size = TOKENS_BUFFER_SIZE;
+	char **tokens = malloc(sizeof(char *) * buffer_size);
+	char *token;
+	int pos = 0;
+
+	if (!tokens)
+	{
+		perror("Could not allocate space for tokens\n");
+		exit(0);
+	}
+	token = strtok(line, TOKEN_DELIMITERS);
+	while (token)
+	{
+		tokens[pos] = token;
+		token = strtok(NULL, TOKEN_DELIMITERS);
+		pos++;
+	}
+	tokens[pos] = NULL;
+	return (tokens);
+}
